952110b482bdda6f8aa48caaead2861ca24a335d
FrosterDune <kiminek005@gmail.com>
2023-01-26 03:56:45 +0100

    Add project files.

---
+++ LogisticCalculationWPF/Model/KalkulaceModel.cs
@@ -0,0 +1,43 @@
+﻿using LogisticCalculationWPF.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogisticCalculationWPF.Model
+{
+    public class KalkulaceModel
+    {
+        private double? VelikostPoptavky;
+        private double? Npz;
+        private double? Ns;
+        private double? Nj;
+        private double? Obdobi;
+
+        public KalkulaceModel(double? velikostPoptavky, double? npz, double? ns, double? nj, double? obdobi) 
+        { 
+            VelikostPoptavky= velikostPoptavky;
+            Npz= npz;
+            Ns= ns;
+            Nj= nj;
+            Obdobi= obdobi;
+        }
+
+        public double? Qopt()
+        {
+            return Math.Ceiling(Math.Sqrt(2 * (VelikostPoptavky ?? 0) * (Npz ?? 0)) / Math.Sqrt((Nj ?? 0) * (Ns ?? 0) * (Obdobi ?? 0)));
+        }
+        
+        public double? PocetDavek()
+        {
+            return Math.Ceiling((VelikostPoptavky ?? 0) / (Qopt() ?? 0));
+
+        }
+        
+        public double? PeriodicitaZadavani()
+        {
+            return Math.Ceiling((360 * (Obdobi ?? 0)) / (PocetDavek() ?? 0));
+        }
+    }
+}
---
+++ LogisticCalculationWPF/ViewModel/QoptDavkaViewModel.cs
@@ -0,0 +1,81 @@
+﻿using LogisticCalculationWPF.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using CommunityToolkit.Mvvm.Input;
+using System.Security.Permissions;
+
+namespace LogisticCalculationWPF.ViewModel
+{
+    class QoptDavkaViewModel: INotifyPropertyChanged
+    {
+        private KalkulaceModel kalkulace;
+        private double? davka;
+        private double? npz;
+        private double? ns;
+        private double? nj;
+        private double? obdobi;
+        private string vysledek;
+
+        public QoptDavkaViewModel()
+        {
+            VypocitejQoptBt = new RelayCommand(Kalkulace);
+            Davka = null;
+            Npz = null;
+            Ns = null;
+            Nj = null;
+            Obdobi = null;
+
+        }
+        public double? Davka 
+        { 
+            get { return davka; } 
+            set { davka = value; OnPropertyChanged(nameof(Davka)); } 
+        }
+        public double? Npz 
+        { 
+            get { return npz; }
+            set { npz = value; OnPropertyChanged(nameof(Npz)); } 
+        }
+        public double? Ns {
+            get { return ns; } 
+            set { ns = value; OnPropertyChanged(nameof(Ns)); } 
+        }
+        public double? Nj {
+            get { return nj; } 
+            set { nj = value; OnPropertyChanged(nameof(Nj)); } 
+        }
+        public double? Obdobi {
+            get { return obdobi; } 
+            set { obdobi = value; OnPropertyChanged(nameof(obdobi)); } 
+        }
+        public string VysledekQopt 
+        { 
+            get { return vysledek; } 
+            set { vysledek = value; OnPropertyChanged(nameof(VysledekQopt)); } 
+        }
+        public ICommand VypocitejQoptBt { get; set; }
+
+        private void Kalkulace()
+        {
+            kalkulace = new KalkulaceModel(davka, npz, ns, nj, obdobi);
+            VysledekQopt = 
+                $"Optimální dávka: {kalkulace.Qopt()}\r\n" +
+                $"Počet dávek: {kalkulace.PocetDavek()}\r\n" +
+                $"Periodicita Zadávání: {kalkulace.PeriodicitaZadavani()} dnů (360 dnů)";
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName) 
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+    }
+}

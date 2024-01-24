c6a4767a3328e2e4720506c3885c178323fc1430
VtrStla <kiminek005@gmail.com>
2024-01-24 19:21:56 +0100

    Add UnitTest template and update SqlClient because of CVE-2024-0056

---
+++ LogisticCalculationTests/UnitTest1.cs
@@ -0,0 +1,21 @@
+using System;
+using LogisticCalculationWPF;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LogisticCalculationTests
+{
+    [TestClass]
+    public class UnitTest1
+    {        
+        [TestMethod]
+        public void TestMethod1()
+        {
+            
+        }
+        [TestCleanup] 
+        public void Cleanup() 
+        { 
+        
+        }
+    }
+}
